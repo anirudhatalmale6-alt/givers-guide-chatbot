c565bee81364dd2f7e7f4b31ab3cf6921604b02e
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-03-06 03:49:24 +0000

    Fix send button icon visibility - SVG was flex-shrinking to 0px width

--- templates/chatbot-widget.php
+++ templates/chatbot-widget.php
@@ -44,7 +44,7 @@
             <form id="gg-chatbot-form" class="gg-chatbot-form">
                 <input type="text" id="gg-chatbot-input" class="gg-chatbot-input" placeholder="Type your message..." autocomplete="off" />
                 <button type="submit" class="gg-chatbot-send" aria-label="Send message">
-                    <svg viewBox="0 0 24 24" fill="currentColor"><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>
+                    <svg viewBox="0 0 24 24" fill="currentColor"><path d="M3.4 20.4l17.45-7.48a1 1 0 0 0 0-1.84L3.4 3.6a.993.993 0 0 0-1.39.91L2 9.12c0 .5.37.93.87.99L17 12 2.87 13.88c-.5.07-.87.5-.87 1l.01 4.61c0 .71.73 1.2 1.39.91z"/></svg>
                 </button>
             </form>
             <div class="gg-chatbot-powered">Powered by Givers' Guide</div>
